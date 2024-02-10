28d67c238de0c2b752780c07d76a92ad7f62e709
Ezekiel A. Mitchell <135088287+ezekielmitchell@users.noreply.github.com>
2024-02-10 13:45:54 -0800

    Timing format
    Changed timing format to minutes

--- pomodoro.ino
+++ pomodoro.ino
@@ -4,17 +4,14 @@ extern uint8_t BigFont[];
 
 UTFT myGLCD(ILI9486,38,39,40,41);
 
-int long countdownDuration = 45 * 60 * 1000;
+int long countdownDuration = 45;
 
 void setup() {
-  Serial.begin(9600); // Start serial communication for debugging
   randomSeed(analogRead(0));
   
-  // Setup the LCD
   myGLCD.InitLCD();
   myGLCD.setFont(BigFont);
 
-  // Clear the screen and draw the frame
   myGLCD.clrScr();
   myGLCD.fillScr(255, 255, 255);
   myGLCD.setColor(0, 0, 0);
@@ -22,19 +19,26 @@ void setup() {
   myGLCD.print("HELLO, EZEKIEL", CENTER, 100, 0);
   myGLCD.print("WORK HARDER", CENTER, 130, 0);
 
-
 }
 
 void loop() {
 
-  countdownDuration = countdownDuration - 1;
-  
-  String timer = String(countdownDuration);
   myGLCD.setColor(0, 0, 0);
   myGLCD.setBackColor(255, 255, 255);
-  myGLCD.print(timer, CENTER, 175, 0);
-  delay(1000);
-  
 
+  if (countdownDuration <= 0) {
+
+    myGLCD.print("BREAK TIME!", CENTER, 175, 0);
+
+  } else {
+
+    countdownDuration = countdownDuration - 1;
+    String timer = String(countdownDuration);
+    myGLCD.print(timer, CENTER, 175, 0);
+
+  }
+
+  delay(1000 * 60);
+  
 }
 
